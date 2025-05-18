df9ac0181413c32b2944187125ead801bb624504
Rey Ramos Tuñacao <rey52090@gmail.com>
2025-05-18 12:46:49 +0800

    added loader

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.getElementById('loader-overlay').classList.add('hide');
+  }, 2000); // 2 seconds minimum load time
+});
