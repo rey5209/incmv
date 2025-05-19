628c361bc977afe86c13bdc5d9edea941c0b96c2
Rey Ramos Tuñacao <rey52090@gmail.com>
2025-05-19 12:21:48 +0800

    update watch.html

---
+++ js/slide-loader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.getElementById('slide-loader-overlay').classList.add('hide');
+  }, 900); // Wait for slide animation to finish
+});
